e3e2956f2c2b7e1a9b6e93ed8d746e68dbc0cff4
Ahmadfauzan28 <82116012+Ahmadfauzan28@users.noreply.github.com>
2021-08-11 07:26:50 +0700

    Update and rename bot pengguna/__init__.py to pengguna bot/__init__.py

--- pengguna bot/__init__.py
+++ pengguna bot/__init__.py
@@ -110,7 +110,7 @@ UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/Ahmadfauzan28/my-zhan")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "RAM-UBOT")
+    "UPSTREAM_REPO_BRANCH", "my-zhan")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
